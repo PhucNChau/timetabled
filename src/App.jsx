d0b596904f976180318a120c65dc3f2c092ff02e
Phuc Chau <phucnchau@gmail.com>
2024-09-17 21:52:17 -0400

    Add new Calendar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Calendar from './components/Calendar';
 
 const App = () => {
 
@@ -6,6 +7,7 @@ const App = () => {
     <div className="App">
       <h1>Itinerary for 7 Days in Chicago</h1>
       <h2>One week in Chicago is wonderful. If want to get to know the city and see all the sights, then spending 7 days in Chicago is perfect!</h2>
+      <Calendar />
     </div>
   )
 }
